4c1b4b419016c997f77de3ebe838d84ac1fe33a2
Romain Meteye <romain.meteye.2017@gmail.com>
2022-12-22 13:44:28 +0100

    ia easy mode

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+var currentPlayer = 1;
 function game() {
     // ###### RESET ######
     let listAllCases = document.getElementsByClassName("col");
@@ -10,13 +11,15 @@ function game() {
     };
 
     // ###### INITIALISATION ######
-    let currentPlayer = 1;
     let gridP1 = [];
     let gridP2 = [];
     let grid = [];
+    let iaPoss = [];
+    currentPlayer = 1;
     let listCases = document.getElementsByClassName("case");
     let winText = document.getElementById("winText");
     let turnText = document.getElementById("turnText");
+    iaPoss =['0','1','2','3','4','5','6','7','8'];
     turnText.innerHTML = "C'est à X de Jouer !";
     winText.innerHTML = "";
 
@@ -41,10 +44,13 @@ function game() {
                 winText.innerHTML = "ÉGALITÉ !";
             };
         };
-        // ###### TEST CLICK & ACTION THE GAME ######
+        // ###### TEST CLICK & ACTIONS OF GAME ######
         getCase.onclick = function() {
             grid[getCase.classList[2]] = currentPlayer;
             console.log(grid);
+            let index = iaPoss.indexOf(getCase.classList[2]);
+            iaPoss.splice(index, 1);
+            // ###### PLAYER 1 ######
             if (currentPlayer === 1 && getCase.childNodes.length === 0) {
                 this.innerHTML = "X";
                 gridP1.push(getCase.classList[2]);
@@ -54,18 +60,47 @@ function game() {
                 testEnd();
                 currentPlayer ++;
                 
+                easy(iaPoss,testEnd,gridP2,listCases,turnText,grid);
+            // ###### PLAYER 2 ######
             } else if (currentPlayer === 2 && getCase.childNodes.length === 0) {
-                this.innerHTML = "O";
-                gridP2.push(getCase.classList[2]);
-                this.setAttribute("class","col done");
-                listCases = document.getElementsByClassName("case");
-                turnText.innerHTML = "C'est à X de Jouer !";
-                testEnd();
-                currentPlayer --;
+                // secondPlayer(getCase,testEnd,gridP2,listCases,turnText);
             };
         };
     };
 };
 
-game();
+function easy(iaPoss,testEnd,gridP2,listCases,turnText,grid) {
+    console.log(`Possibilité restantes : ${iaPoss}`);
+    let iaPlay = Math.floor(Math.random() * iaPoss.length);
+    console.log(`Chiffre récupéré : ${iaPoss[iaPlay]}`);
+    let getClass = document.getElementsByClassName(iaPoss[iaPlay])[0];
+    if (getClass.childNodes.length === 0) {
+    let index = iaPoss.indexOf(getClass.classList[2]);
+    grid[getClass.classList[2]] = currentPlayer;
+    iaPoss.splice(index, 1);
+    getClass.innerHTML = "O";
+    gridP2.push(getClass.classList[2]);
+    getClass.setAttribute("class","col done");
+    listCases = document.getElementsByClassName("case");
+    turnText.innerHTML = "C'est à X de Jouer !";
+    console.log(`Possibilité restantes : ${iaPoss}`);
+    testEnd();
+    currentPlayer --; };
+    return currentPlayer, gridP2, listCases, turnText, grid;
 
+};
+
+function secondPlayer(getCase,testEnd,gridP2,listCases,turnText) {
+    getCase.innerHTML = "O";
+    gridP2.push(getCase.classList[2]);
+    getCase.setAttribute("class","col done");
+    listCases = document.getElementsByClassName("case");
+    turnText.innerHTML = "C'est à X de Jouer !";
+    testEnd();
+    currentPlayer --;
+    return currentPlayer, gridP2, listCases, turnText;
+}
+
+
+// ###### LAUNCH GAME ON LOADING PAGE ######
+game();
